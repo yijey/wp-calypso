64dfa5b22a5c04a5aa3dbe06660a0eeb4b6c3439
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-01-23 13:40:21 -1000

    Update to use the new syntext

--- client/my-sites/stats/activity-log-item/index.jsx
+++ client/my-sites/stats/activity-log-item/index.jsx
@@ -1,8 +1,13 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { localize } from 'i18n-calypso';
+import classNames from 'classnames';
+import {
+	noop,
+} from 'lodash';
+
 /**
  * Internal dependencies
  */
@@ -11,17 +16,36 @@ import Button from 'components/button';
 import Gridicon from 'components/gridicon';
 import Gravatar from 'components/gravatar';
 
-const ActivityLogItem = React.createClass( {
+
+class ActivityLogItem extends Component {
+
+	static propTypes = {
+		title: PropTypes.string,
+		subTitle: PropTypes.string,
+		className: PropTypes.string,
+		icon: PropTypes.string,
+		user: PropTypes.object,
+		onClick: PropTypes.func,
+		actionText: PropTypes.string,
+		time:  PropTypes.string,
+	};
+
+	static defaultProps = {
+		onClick: noop,
+	};
+
 	getTime() {
 		const {
+			time
 		} = this.props;
 
 		return (
 			<div className="activity-log-item__time">
-				4:21pm
+				{ time }
 			</div>
 		);
-	},
+	}
+
 	getIcon() {
 		const {
 			icon,
@@ -34,7 +58,7 @@ const ActivityLogItem = React.createClass( {
 				</div>
 			</div>
 		);
-	},
+	}
 
 	getActor() {
 		const {
@@ -44,38 +68,50 @@ const ActivityLogItem = React.createClass( {
 		return (
 			<div className="activity-log-item__actor">
 				<Gravatar user={ user } size={ 18 } />
-				<div className="activity-item__actor-name">Vistor</div>
-				<div className="activity-item__actor-role">Vistor</div>
+				{ user && <div className="activity-item__actor-name">{ user.name }</div> }
+				{ user && <div className="activity-item__actor-role">{ user.role }</div> }
 			</div>
 		);
-	},
+	}
 
 	getContent() {
 		const {
+			title,
+			subTitle
 		} = this.props;
 
 		return (
 			<div className="activity-log-item__content">
-				<div className="activity-item__content-main">Tread found in Plugin</div>
-				<div className="activity-item__content-sub">Malware code detected...</div>
+				<div className="activity-item__content-title">{ title }</div>
+				{ subTitle && <div className="activity-item__sub-title">{ subTitle }</div> }
 			</div>
 		);
-	},
+	}
 
 	getAction() {
 		const {
+			onClick,
+			actionText
 		} = this.props;
 
-		return (
+		return ( actionText &&
 			<div className="activity-log-item__action">
-				<Button>Do Stuff</Button>
+				<Button compact primary onClick={ onClick }>{ actionText }</Button>
 			</div>
 		);
-	},
+	}
 
 	render() {
+		const {
+			className,
+		} = this.props;
+
+		const classes = classNames(
+			'activity-log-item',
+			className
+		);
 		return (
-			<div className="activity-log-item" >
+			<div className={ classes } >
 				<div className="activity-log-item__type">
 				{ this.getTime() }
 				{ this.getIcon() }
@@ -88,6 +124,6 @@ const ActivityLogItem = React.createClass( {
 			</div>
 		);
 	}
-} );
+};
 
 export default localize( ActivityLogItem );

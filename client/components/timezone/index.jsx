c7b4f84f75806512750207b3c92b8a16216b5c11
retrofox <rdsuarez@gmail.com>
2017-01-24 16:31:27 -0300

    timezones: refact component - Use QueryTimezones data component - fix ESlint warnings - ES6ify

--- client/components/timezone/index.jsx
+++ client/components/timezone/index.jsx
@@ -1,107 +1,95 @@
 /**
  * External Dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
 import map from 'lodash/map';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import wpcom from 'lib/wp';
+import QueryTimezones from 'components/data/query-timezones';
+import { timezonesRequestAction as requestTimezones } from 'state/timezones/actions';
+import {
+	getRawOffsets,
+	getTimezonesByContinent,
+} from 'state/selectors';
 
 /**
  * Module variables
  */
-const { Component, PropTypes } = React;
 const noop = () => {};
-const undocumented = wpcom.undocumented();
 
 class Timezone extends Component {
-	constructor() {
-		super();
+	static propTypes = {
+		selectedZone: PropTypes.string,
+		onSelect: PropTypes.func
+	};
 
-		// bound methods
-		this.onSelect = this.onSelect.bind( this );
+	static defaultProps = {
+		selectedZone: '',
+		onSelect: noop
+	};
 
-		this.state = {
-			zonesByContinent: [],
-			manualUtcOffsets: []
-		};
+	selectTimezone = ( { target } ) => {
+		this.props.onSelect( target.value );
 	}
 
-	componentWillMount() {
-		undocumented.timezones( ( err, zones ) => {
-			if ( err ) {
-				return;
-			}
-
-			const timezones = {
-				zonesByContinent: zones.timezones_by_continent,
-				manualUtcOffsets: zones.manual_utc_offsets
-			};
-
-			this.setState( timezones );
-		} );
-	}
-
-	onSelect( event ) {
-		this.props.onSelect( event.target.value );
-	}
-
-	renderOptionsByContinent() {
-		const { zonesByContinent } = this.state;
+	renderTimezonesByContinent() {
+		const { timezonesByContinent } = this.props;
 
 		return (
-			map( zonesByContinent, ( countries, continent ) => {
-				return (
-					<optgroup label={ continent } key={ continent }>
-						{
-							map( countries, ( { value, label }, index ) => {
-								return (
-									<option value={ value } key={ index }>{ label }</option>
-								);
-							} )
-						}
-					</optgroup>
-				);
-			} )
+			map( timezonesByContinent, ( countries, continent ) =>
+				<optgroup label={ continent } key={ continent }>
+					{ map( countries, ( { value, label }, index ) =>
+						<option value={ value } key={ index }>{ label }</option>
+					) }
+				</optgroup>
+			)
 		);
 	}
 
 	renderManualUtcOffsets() {
+		const { rawOffsets } = this.props;
 		return (
 			<optgroup label={ this.props.translate( 'Manual Offsets' ) }>
-				{
-					map( this.state.manualUtcOffsets, ( { value, label }, index ) => {
-						return ( <option value={ value } key={ index }>{ label }</option> );
-					} )
-				}
+				{ map( rawOffsets, ( { value, label }, index ) =>
+					<option value={ value } key={ index }>{ label }</option>
+				) }
 			</optgroup>
 		);
 	}
 
 	render() {
 		const { selectedZone } = this.props;
+
 		return (
-			<select onChange={ this.onSelect } value={ selectedZone || '' }>
-				{ this.renderOptionsByContinent() }
-				<optgroup label="UTC">
-					<option value="UTC">UTC</option>
-				</optgroup>
-				{ this.renderManualUtcOffsets() }
-			</select>
+			<div>
+				<QueryTimezones />
+				<select onChange={ this.selectTimezone } value={ selectedZone }>
+					{ this.renderTimezonesByContinent() }
+					<optgroup label="UTC">
+						<option value="UTC">UTC</option>
+					</optgroup>
+					{ this.renderManualUtcOffsets() }
+				</select>
+			</div>
 		);
 	}
 }
 
-Timezone.defaultProps = {
-	onSelect: noop
-};
+/**
+ * - Component-Data connection.
+ * - Localize
+ */
 
-Timezone.propTypes = {
-	selectedZone: PropTypes.string,
-	onSelect: PropTypes.func
-};
+const mapStateToProps = state => ( {
+	rawOffsets: getRawOffsets( state ),
+	timezonesByContinent: getTimezonesByContinent( state ),
+} );
 
-export default localize( Timezone );
+const mapDispatchToProps = { requestTimezones };
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( Timezone ) );
